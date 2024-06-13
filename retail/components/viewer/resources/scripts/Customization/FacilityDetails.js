64c9020650319d8a173879ad7b47307bc7e3691e
Rabindra Adhikari <arabin007@outlook.com>
2024-06-13 10:50:35 +0545

    Initial Upload

---
+++ retail/components/viewer/resources/scripts/Customization/FacilityDetails.js
@@ -0,0 +1,15 @@
+console.log('Inside FacilityDetails');
+
+function showHideChecklistByFacility(){
+
+	//Hiding all frames
+	var frameSections = ['Product1','Product2','Product3','Product4','Product5'];
+	for(let i = 0; i < frameSections.length; i++)
+	{
+		setStyle(frameSections[i],"visible","false"); 
+	}
+	
+	//Showing specific frame
+	let facilityProduct = getValue("FACILITYPRODUCT");
+	setStyle("Product"+facilityProduct,"visible","true");
+}
